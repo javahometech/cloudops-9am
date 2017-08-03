7a8293f1040bd59da3a08bffaa08485e3be721c8
Hari Reddy <hari@javahome.in>
2017-08-03 20:39:41 +0530

    adding s3 backend

--- launch_ec2.tf
+++ launch_ec2.tf
@@ -10,3 +10,11 @@ resource "aws_instance" "demo-ec2" {
       Name = "TerraformDemo"
     }
 }
+
+terraform {
+  backend "s3" {
+    bucket = "cloudops-terraform-state-prod"
+    key    = "cloudops"
+    region = "us-west-2"
+  }
+}
