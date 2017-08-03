f844a63d49c2e87174f2f12b03c57cff7f5712e1
kammana <kammana@kammanas-MacBook-Air.local>
2017-08-03 10:31:13 +0530

    terraform firt example

---
+++ launch_ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_KEY}"
+  region = "${var.AWS_REGION}"
+}
+resource "aws_instance" "demo-ec2" {
+    ami = "${var.AMI_ID}"
+    instance_type = "${var.AWS_INSTANCE_TYPE}"
+    tags {
+      Name = "TerraformDemo"
+    }
+}
---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "AMI_ID" {
+  default = "ami-6df1e514"
+}
+variable "AWS_INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+variable "AWS_ACCESS_KEY" {
+  default = "AKIAIS5IDH2ZFC65TIWA"
+}
+variable "AWS_SECRET_KEY" {
+  default = "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"
+}
